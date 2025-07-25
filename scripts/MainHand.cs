2e76db1c0d574ef0d02e65f9ae6022063e2e3621
Jeremy A. Cunningham <jac@kennebel.com>
2025-07-25 17:34:52 -0600

    Actual slow indicator

--- scripts/MainHand.cs
+++ scripts/MainHand.cs
@@ -7,6 +7,7 @@ public partial class MainHand : Node2D
 
     public override void _Process(double delta)
     {
-        this.SetRotation(((float)DateTime.Now.Second / 60f) * TwoPi);
+        //this.SetRotation(((float)DateTime.Now.Second / 60f) * TwoPi);
+        this.SetRotation(((float)DateTime.Now.TimeOfDay.TotalSeconds / 86400f) * TwoPi);
     }
 }
