b237825a27ea8ed7bc9ac155f9c62cd476c48259
Jeremy A. Cunningham <jac@kennebel.com>
2025-07-25 16:23:37 -0600

    Initial check in

---
+++ scripts/MainHand.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class MainHand : Node2D
+{
+    private const float TwoPi = (float)Math.PI * 2f;
+
+    public override void _Process(double delta)
+    {
+        this.SetRotation(((float)DateTime.Now.Second / 60f) * TwoPi);
+    }
+}
